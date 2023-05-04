81bc40187b4e1b7e5069cf42a542d5b0e76038c9
Mario Eugenio <mgomes@gvdasa.com.br>
2023-05-04 14:02:32 -0300

    add project

---
+++ src/test/java/com/example/ugfarquiteturapoc/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ugfarquiteturapoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
